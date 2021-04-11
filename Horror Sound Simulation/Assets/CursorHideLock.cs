7cc31ebbb27d21515398efb45513245536c4a5c5
Kevin Samuelsson <keruwihn@gmail.com>
2021-04-11 18:16:08 +0200

    le epic pilot
    3D ljud, puzzle fix, mus/WASD bajs

---
+++ Horror Sound Simulation/Assets/CursorHideLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHideLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
